f5da25c9b385fbb7c25ccf790f0d558f83d696cd
Gledson Rwalker <administrativo@homeassiste.com>
2026-03-16 18:42:31 -0300

    Muda a senha do administrador

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ except locale.Error:
 app = Flask(__name__)
 app.secret_key = 'minha_chave_secreta_muito_segura_12345'
 app.config['CERTIFICATE_FOLDER'] = 'certificates'
-ADMIN_PASSWORD = "admin" 
+ADMIN_PASSWORD = "Leve@2020" 
 
 # --- FUNÇÃO DE CONEXÃO COM O BANCO DE DADOS ---
 def get_db_connection():
